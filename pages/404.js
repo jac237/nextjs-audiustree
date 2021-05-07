838f9c5407331ebd87a73fc5e7ec1f915037e10c
Jessie Cruz <jac237@ucsd.edu>
2021-05-06 21:18:09 -0700

    Basic Error Pages

---
+++ pages/404.js
@@ -0,0 +1,62 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+// pages/404.js
+export default function Custom404() {
+  return (
+    <div>
+      <Head>
+        <title>404 | AudiusTree</title>
+        <meta name="description" content="#MOOMBAHFY" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container maxWidth="md">
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          style={{ padding: '100px 0px 150px' }}
+        >
+          <Grid item>
+            <img
+              src="https://i.imgur.com/Feg2bVU.png"
+              style={{ height: 120, width: 120 }}
+            />
+          </Grid>
+
+          <Grid item>
+            <Typography
+              variant="inherit"
+              component="h1"
+              align="center"
+              style={{ textTransform: 'uppercase' }}
+            >
+              This page no longer exists or ever existed
+            </Typography>
+          </Grid>
+
+          <Grid item>
+            <Typography
+              variant="subtitle1"
+              align="center"
+              color="textSecondary"
+              style={{ fontWeight: 500 }}
+            >
+              Head back to the{' '}
+              <span
+                style={{ textDecoration: 'underline', color: 'whitesmoke' }}
+              >
+                <Link href="/">HOMEPAGE</Link>
+              </span>
+              .
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
