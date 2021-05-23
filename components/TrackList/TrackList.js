599fbb21ac8b161d03d3db4236e6f02e77d1b745
Jessie Cruz <jac237@ucsd.edu>
2021-05-22 21:10:26 -0700

    components/TrackList: Completed Layout

--- components/TrackList/TrackList.js
+++ components/TrackList/TrackList.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import Link from 'next/link';
+import { makeStyles } from '@material-ui/core/styles';
 import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentTrack, selectCurrentTrack } from '../../redux/playerSlice';
 import styles from './TrackList.module.css';
+import approx from 'approximate-number';
 import styled from 'styled-components';
 import TimeFormat from 'hh-mm-ss';
 import List from '@material-ui/core/List';
@@ -22,13 +24,40 @@ import PersonIcon from '@material-ui/icons/Person';
 import CodeIcon from '@material-ui/icons/Code';
 import EqualizerIcon from '@material-ui/icons/Equalizer';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import PlayArrowRoundedIcon from '@material-ui/icons/PlayArrowRounded';
 import Skeleton from '@material-ui/lab/Skeleton';
 import TrackDialog from '../MetadataDialogs/TrackDialog';
 import getTweetIntent from '../../lib/getTweetIntent';
 
+const useStyles = makeStyles((theme) => ({
+  listAvatar: {
+    minWidth: 75,
+    [theme.breakpoints.up('sm')]: {
+      minWidth: 95,
+    },
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 4,
+    [theme.breakpoints.up('sm')]: {
+      width: 80,
+      height: 80,
+    },
+  },
+  albumIcon: {
+    width: 50,
+    height: 50,
+    [theme.breakpoints.up('sm')]: {
+      width: 70,
+      height: 70,
+    },
+  },
+}));
+
 export default function TrackList({ tracks }) {
   return (
-    <List>
+    <List style={{ width: '100%' }}>
       {Boolean(tracks)
         ? tracks.map((track) => <TrackListItem track={track} key={track.id} />)
         : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((index) => (
@@ -44,10 +73,12 @@ const initialState = {
 };
 
 const TrackListItem = ({ track }) => {
+  const classes = useStyles();
   const currentTrack = useSelector(selectCurrentTrack);
   const dispatch = useDispatch();
   const [state, setState] = useState(initialState);
   const [anchorEl, setAnchorEl] = useState(null);
+  const [isDialogOpen, setDialogOpen] = useState(false);
 
   const handleMenuClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -74,6 +105,57 @@ const TrackListItem = ({ track }) => {
     setState(initialState);
   };
 
+  const MenuItems = ({ track, onMenuClose }) => {
+    const handleDialogClick = () => {
+      onMenuClose();
+      setDialogOpen(true);
+    };
+
+    const handleDialogClose = () => {
+      setDialogOpen(false);
+    };
+
+    return (
+      <>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={getTweetIntent(track.id)}
+        >
+          <MenuItem onClick={onMenuClose}>
+            <TwitterIcon className={styles.menuIcon} />
+            <Typography variant="subtitle2">Share</Typography>
+          </MenuItem>
+        </a>
+
+        <MenuItem onClick={handleDialogClick}>
+          <CodeIcon className={styles.menuIcon} />
+          <Typography variant="subtitle2">View Metadata</Typography>
+        </MenuItem>
+
+        <Link href={`/user/${track.user.handle}`}>
+          <MenuItem onClick={onMenuClose}>
+            <PersonIcon className={styles.menuIcon} />
+            <Typography variant="subtitle2">View Artist</Typography>
+          </MenuItem>
+        </Link>
+
+        <Link href={`/tracks/${track.id}`}>
+          <MenuItem onClick={onMenuClose}>
+            <AlbumIcon className={styles.menuIcon} />
+            <Typography variant="subtitle2">View Track</Typography>
+          </MenuItem>
+        </Link>
+
+        <TrackDialog
+          track={track}
+          open={isDialogOpen}
+          handleClose={handleDialogClose}
+        />
+      </>
+    );
+  };
+
   return (
     <>
       <ListItem
@@ -81,40 +163,29 @@ const TrackListItem = ({ track }) => {
         onClick={() => handleTrackClick(track)}
         onContextMenu={handleRightClick}
       >
-        <ListItemAvatar style={{ minWidth: 60 }}>
+        <ListItemAvatar className={classes.listAvatar}>
           {track.artwork ? (
             <Avatar
               variant="square"
               src={track.artwork['150x150']}
-              style={{ width: 48, height: 48 }}
+              className={classes.avatar}
             />
           ) : (
-            <Avatar variant="square" style={{ width: 48, height: 48 }}>
-              <AlbumIcon style={{ height: 40, width: 40 }} />
+            <Avatar variant="square" className={classes.avatar}>
+              <AlbumIcon className={classes.albumIcon} />
             </Avatar>
           )}
         </ListItemAvatar>
 
         <ListItemText
+          style={{ width: '100%' }}
           primary={
             <Typography variant="inherit" component="h3" noWrap>
               {track.title}
             </Typography>
           }
           secondary={
-            currentTrack?.id === track.id ? (
-              <Typography
-                variant="subtitle2"
-                className={styles.nowPlaying}
-                noWrap
-              >
-                <EqualizerIcon
-                  style={{ marginRight: 4, fontSize: 18 }}
-                  color="inherit"
-                />
-                Now Playing
-              </Typography>
-            ) : (
+            <div>
               <Typography
                 variant="subtitle2"
                 color="textSecondary"
@@ -124,11 +195,45 @@ const TrackListItem = ({ track }) => {
                 {track.user.name}
                 {track.user.is_verified && (
                   <CheckCircleIcon
-                    style={{ color: '#02e976', fontSize: 14, marginLeft: 4 }}
+                    style={{
+                      color: '#02e976',
+                      fontSize: 14,
+                      marginLeft: 4,
+                    }}
                   />
                 )}
               </Typography>
-            )
+              {currentTrack?.id === track.id ? (
+                <Typography
+                  variant="subtitle2"
+                  className={styles.nowPlaying}
+                  noWrap
+                >
+                  <EqualizerIcon
+                    style={{ marginRight: 4, fontSize: 18 }}
+                    color="inherit"
+                  />
+                  Now Playing
+                </Typography>
+              ) : (
+                <Typography
+                  variant="subtitle2"
+                  color="textSecondary"
+                  noWrap
+                  style={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <PlayArrowRoundedIcon
+                    style={{
+                      color: 'inherit',
+                      fontSize: 20,
+                      marginRight: 4,
+                      marginLeft: '-4px',
+                    }}
+                  />
+                  {approx(track.play_count)}
+                </Typography>
+              )}
+            </div>
           }
         />
 
@@ -140,6 +245,7 @@ const TrackListItem = ({ track }) => {
               width: 18,
               height: 18,
               marginRight: 8,
+              marginLeft: 4,
             }}
           />
         )}
@@ -200,64 +306,13 @@ const TrackListItem = ({ track }) => {
   );
 };
 
-const MenuItems = ({ track, onMenuClose }) => {
-  const [isDialogOpen, setDialogOpen] = useState(false);
-
-  const handleDialogClick = () => {
-    onMenuClose();
-    setDialogOpen(true);
-  };
-
-  const handleDialogClose = () => {
-    setDialogOpen(false);
-  };
-
-  return (
-    <>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href={getTweetIntent(track.id)}
-      >
-        <MenuItem onClick={onMenuClose}>
-          <TwitterIcon className={styles.menuIcon} />
-          <Typography variant="subtitle2">Share</Typography>
-        </MenuItem>
-      </a>
-
-      <Link href={`/tracks/${track.id}`}>
-        <MenuItem onClick={onMenuClose}>
-          <AlbumIcon className={styles.menuIcon} />
-          <Typography variant="subtitle2">View Track</Typography>
-        </MenuItem>
-      </Link>
-
-      <Link href={`/user/${track.user.handle}`}>
-        <MenuItem onClick={onMenuClose}>
-          <PersonIcon className={styles.menuIcon} />
-          <Typography variant="subtitle2">View Artist</Typography>
-        </MenuItem>
-      </Link>
-
-      <MenuItem onClick={handleDialogClick}>
-        <CodeIcon className={styles.menuIcon} />
-        <Typography variant="subtitle2">View Metadata</Typography>
-      </MenuItem>
-
-      <TrackDialog
-        track={track}
-        open={isDialogOpen}
-        handleClose={handleDialogClose}
-      />
-    </>
-  );
-};
-
 const LoadingListItem = () => {
+  const classes = useStyles();
+
   return (
     <ListItem button>
-      <ListItemAvatar>
-        <Skeleton animation="wave" variant="rect" width={40} height={40} />
+      <ListItemAvatar className={classes.listAvatar}>
+        <Skeleton animation="wave" variant="rect" className={classes.avatar} />
       </ListItemAvatar>
 
       <ListItemText
@@ -267,10 +322,3 @@ const LoadingListItem = () => {
     </ListItem>
   );
 };
-
-const UserLink = styled.div`
-  &:hover {
-    text-decoration: underline;
-    color: whitesmoke;
-  }
-`;
